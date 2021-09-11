08d73b9b549b57e53c7473ac89a23760d0d1cef4
Zach <zbehan@gmail.com>
2021-09-12 09:49:12 +1000

    Initial pseudocode

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,48 @@
-console.log("Hello world");
+// rock beats scissors
+// paper beats rock
+// scissors beats paper
+
+// Create variable choicesArr of type Array that starts with the values: rock, paper, scissors
+
+// **Computer chooses from rock, paper or scissors
+
+// getComputerChoice is a function that takes choicesArr as a parameter
+// choicesArr is an array
+
+// Create variable computerChoice of type String that starts with a blank string
+// Create variable randNum of type Integer that starts with a random number between 0 and length of choicesArr
+// Assign value at position of randNum in choicesArr to computerChoice
+// return computerChoice
+
+
+
+// **Player chooses from rock, paper or scissors
+
+// getPlayerChoice is a function that takes choicesArr as a parameter
+// choicesArr is an array
+
+// Create variable playerInput of type String that starts with a blank string
+// Output what options the player has to select from
+// Ask player which option they select and store in playerInput
+// Convert playerInput to lowercase
+
+// while playerInput is not in choicesArr
+    // ask user again for selection
+
+// return playerInput
+
+
+// **Check who won
+
+// checkWhoWon is a function that takes playerChoice and computerChoice as parameters
+// playerChoice and computerChoice are Strings
+
+// if computerChoice and playerChoice are the same
+    // draw
+// if computerChoice is rock and playerChoice is scissors
+    // computer wins
+// if computerChoice is paper and playerChoice is rock
+    // computer wins
+// if computerChoice is scissors and playerChoice is paper
+    // computer wins
+// else players wins
