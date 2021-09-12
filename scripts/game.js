1f996a07f05062614fcafbd70d1030011ff9970f
Zach <zbehan@gmail.com>
2021-09-12 17:21:12 +1000

    Refactor checkWhoWon to return simple string values

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,3 @@
-// rock beats scissors
-// paper beats rock
-// scissors beats paper
-
-// Create variable choicesArr of type Array that starts with the values: rock, paper, scissors
-const CHOICES = ["rock", "paper", "scissors"];
-
 // Step 1: Computer chooses from rock, paper or scissors
 // getComputerChoice is a function that takes choicesArr as a parameter
 // choicesArr is an array
@@ -51,53 +44,55 @@ function getPlayerChoice(CHOICES) {
 // playerChoice and computerChoice are Strings
 // checkWhoWon returns a string
 function checkWhoWon(playerChoice, computerChoice) {
-    // Create variable playerWins of type Boolean with start value of true
-    let playerWins = true;
-
     // if computerChoice and playerChoice are the same
     if (playerChoice === computerChoice) {
         // draw
-        return "It's a draw!"
+        return "draw";
         // if computerChoice is rock and playerChoice is scissors
     } else if (computerChoice === "rock" && playerChoice === "scissors") {
         // computer wins
-        playerWins = false;
+        return "computer";
         // if computerChoice is paper and playerChoice is rock
     } else if (computerChoice === "paper" && playerChoice === "rock") {
         // computer wins
-        playerWins = false;
+        return "computer";
         // if computerChoice is scissors and playerChoice is paper
     } else if (computerChoice === "scissors" && playerChoice === "paper") {
         // computer wins
-        playerWins = false;
+        return "computer";
     }
-
-    // if the player wins
-    if (playerWins) {
-        // then return: "You win! playerChoice beats computerChoice"
-        return `You win! ${playerChoice} beats ${computerChoice}.`
-    } else {
-        // otherwise, return "Computer wins! computerChoice beats playerChoice"
-        return `Computer wins. ${computerChoice} beats ${playerChoice}.`
-    }
+    return "player";
 }
 
 
 // Step 4: Play five rounds of the game, keep score and determine an overall winner
 function game() {
+    // Create variable choicesArr of type Array that starts with the values: rock, paper, scissors
+    const CHOICES = ["rock", "paper", "scissors"];
     // Create a variable roundsToPlay of type Number that starts with the value 5
     let roundsToPlay = 5;
 
     // Begin with i at 0 and add 1 to i on each pass until i is equal to or greater than roundsToPlay
     for (let i = 0; i < roundsToPlay; i++) {
         // Inside loop, determine computer and player choices, and the round winner
-        let computerChoice = getComputerChoice(CHOICES)
+        let computerChoice = getComputerChoice(CHOICES);
         console.log(`Computer: ${computerChoice}`);
 
         let playerChoice = getPlayerChoice(CHOICES)
         console.log(`Player: ${playerChoice}`);
 
         let result = checkWhoWon(playerChoice, computerChoice);
+        // Create variables playerScore and computerScore of type Number that start with the value 0
+        // if result is equal to "draw"
+        //    output draw
+        // else if result is equal to "player"
+        //    increase playerScore by 1
+        // else if result is equal to "computer"
+        //    increase computerScore by 1
+
         console.log(result);
     }
 }
+
+// Play the game
+game();
