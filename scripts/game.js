7869ff34e5b5ae9d7ddd77454c48a21c061ed68b
Zach <zbehan@gmail.com>
2021-09-12 10:04:27 +1000

    Create computer choice function

--- scripts/game.js
+++ scripts/game.js
@@ -3,21 +3,24 @@
 // scissors beats paper
 
 // Create variable choicesArr of type Array that starts with the values: rock, paper, scissors
+const CHOICES = ["rock", "paper", "scissors"];
 
-// **Computer chooses from rock, paper or scissors
-
+// Step 1: Computer chooses from rock, paper or scissors
 // getComputerChoice is a function that takes choicesArr as a parameter
 // choicesArr is an array
+function getComputerChoice(CHOICES) {
+    // Create variable computerChoice of type String that starts with a blank string
+    let computerChoice = "";
+    // Create variable randNum of type Integer that starts with a random number between 0 and length of choicesArr
+    let randNum = Math.floor(Math.random() * CHOICES.length);
+    // Assign value at position of randNum in choicesArr to computerChoice
+    computerChoice = CHOICES[randNum];
+    // return computerChoice
+    return computerChoice;
+}
 
-// Create variable computerChoice of type String that starts with a blank string
-// Create variable randNum of type Integer that starts with a random number between 0 and length of choicesArr
-// Assign value at position of randNum in choicesArr to computerChoice
-// return computerChoice
 
-
-
-// **Player chooses from rock, paper or scissors
-
+// Step 2: Player chooses from rock, paper or scissors
 // getPlayerChoice is a function that takes choicesArr as a parameter
 // choicesArr is an array
 
@@ -27,22 +30,28 @@
 // Convert playerInput to lowercase
 
 // while playerInput is not in choicesArr
-    // ask user again for selection
+// ask user again for selection
 
 // return playerInput
 
 
-// **Check who won
+// Step 3: Check who won
 
 // checkWhoWon is a function that takes playerChoice and computerChoice as parameters
 // playerChoice and computerChoice are Strings
 
 // if computerChoice and playerChoice are the same
-    // draw
+// draw
 // if computerChoice is rock and playerChoice is scissors
-    // computer wins
+// computer wins
 // if computerChoice is paper and playerChoice is rock
-    // computer wins
+// computer wins
 // if computerChoice is scissors and playerChoice is paper
-    // computer wins
+// computer wins
 // else players wins
+
+
+// Put it all together
+console.log(getComputerChoice(CHOICES));
+// getPlayerChoice(CHOICES);
+// checkWhoWon();
